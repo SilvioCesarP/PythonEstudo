c3ad500e28b8b7971e27ca03e89dc346f5c4587b
SilvioCesarP <131668165+SilvioCesarP@users.noreply.github.com>
2023-04-24 19:54:01 -0300

    Novos Exercicios

---
+++ Exercícios/Ex012.py
@@ -0,0 +1,3 @@
+preco = float(input('Digite o preço do seu produto: '))
+desconto = preco * 0.05 + preco
+print('Seu produto tem um desconto de 5% sendo o valor total {}, assim dando o valor final {} do produto'.format(preco, desconto))
