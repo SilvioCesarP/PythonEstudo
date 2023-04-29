f8f4a5b103fa5c2aa8c4dec393ba63a7ffc9ff73
SilvioCesarP <131668165+SilvioCesarP@users.noreply.github.com>
2023-04-29 20:35:34 -0300

    projeto

--- Aula15Exercicios/Ex071.py
+++ Aula15Exercicios/Ex071.py
@@ -7,18 +7,21 @@ ced20 = 20
 ced50 = 50
 ced100 = 100
 ced200 = 200
+resto = 0
 
 valor = int(input('Digite o valor que quer sacar! '))
 while True:
-    if valor/200 == 0:
-        ced200 = valor / 200
-    if valor/100 == 0:
+    if valor // 200 > 1:
+        ced200 = valor // 200
+        resto = valor -(ced200*200)
+    if valor/100  > 0:
         ced100 = valor / 100
     if valor / 50 == 0:
         ced50 = valor/50
     if valor / 20 == 0:
         ced20 = valor/20
+    print(ced200,ced100, ced50, ced20)
     break
 
-print(ced200,ced100, ced50, ced20)
+
 
---
+++ ProjetoPessoal/Duascabeçudas.py
@@ -0,0 +1,52 @@
+#3 individuo(galinha, lobo, vaca)
+#Perguntarqual passa primeiro
+#não pode deixar o lobo sozinho com nem umas das duas, galinha ou vaca
+
+
+print('BEM VINDO AO JOGUINHO DO SILVIO')
+print('-='* 20)
+print('VOCE É AMIGO DE TODOS OS ANIMAIS, E CUIDA DE UM LOBO, UMA VACA E UMA GALINHAZ\nPOREM PRECISA IR NA CIDADE COMPRAR COMIDA POIS SEU LOBO ESTA MORRENDO DE FOME')
+print('PARA CHEGAR NA CIDADE PRECISA ATRAVESAR UM RIO, VOCÊ SÓ TEM UM BOTE QUE CABE APENAS 2 PASSAGEIROS\nSE DEIXAR O LOBO SOZINHO COM A VACA OU A GALINHA ELE VAI COMER ELAS :C')
+print('Sendo você o unico que consegue dirigir o bote, vamos considerar cada um com um numero\nLOBO[1]\nGALINHA[2]\nVACA[3]\nVOCE[4]')
+
+morreu = 1
+while True:
+    if morreu == 0:
+        break
+    resp1 = str(input('QUAL ANIMAL ATRAVESSERÀ PRIMEIRO?[1, 2, 3]'))
+    if resp1 == 2 or 3:
+        print('NÃOOOO, VOCE DEIXOU O LOBO SOZINHO COM OS ANIMAIS, ELE COMEU ELES DE TANTA FOME!!!')
+        break     
+    elif resp1 == 1:
+        while True:
+            if morreu == 0:
+                break
+            print('Otimo, agora tem um lobo do outro lado e a galinha e a vaca deste lado')
+            resp2 = str(input('QUAL ANIMAL ATRAVESSERÁ?'))
+            
+            if resp2 == 2:
+                while True:
+                    print('Otimo, agora tem um LOBO e uma GALINHA de um lado e uma VACA do outro')
+                    resp3 = str(input('QUAL ANIMAL ATRAVESSERÁ?'))
+                    if resp3 == 3:
+                        print('NÃOOOO, VOCE DEIXOU O LOBO SOZINHO COM OS ANIMAIS, ELE COMEU ELES DE TANTA FOME!!!')
+                        morreu = 0
+                        break
+                    if resp3 == 1:
+                        print('Voce moveu o LOBO junto a GALINHA, deixando a VACA do outro lado')
+                    if resp3 == 2:
+                        break
+            if resp2 == 3:
+                print('Otimo, agora tem um LOBO e uma VACA de um lado e uma GALINHA do outro')
+            if resp2 == 1:
+                resp = 1
+                    
+
+            
+        
+            
+
+
+
+
+    
